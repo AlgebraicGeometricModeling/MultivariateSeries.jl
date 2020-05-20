90cfa180dc1dac9eabbc1773c0479a468b4358e2
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-20 07:33:49 +0000

    build based on 4ddcbec

--- dev/search_index.js
+++ dev/search_index.js
@@ -321,14 +321,6 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "code/3.decompose/#MultivariateSeries.decompose",
-    "page": "Decomposition",
-    "title": "MultivariateSeries.decompose",
-    "category": "function",
-    "text": "decompose(σ :: Series{C,M}, rkf :: Function)\n\nDecompose the series σ as a weighted sum of exponentials. Return ω, Ξ where\n\nω is the vector of weights,\nΞ is the matrix of frequency points, stored per row.\n\nThe list of monomials of degree leq d-1 over 2 are used to construct the Hankel matrix, where d is the maximal degree of the moments in σ.\n\nThe optional argument rkf is the rank function used to determine the numerical rank from the vector S of singular values. Its default value eps_rkf(1.e-6) determines the rank as the first i s.t. S[i+1]/S[i]< 1.e-6 where S is the vector of singular values.\n\nIf the rank function cst_rkf(r) is used, the SVD is truncated at rank r.\n\n\n\n\n\n"
-},
-
-{
     "location": "code/3.decompose/#Decomposition-1",
     "page": "Decomposition",
     "title": "Decomposition",
